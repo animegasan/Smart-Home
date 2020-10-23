c9e06093c394b1bd2c856d57d678ceb5d338d148
Hilman Maulana <hilman0.0maulana@gmail.com>
2020-10-23 12:02:39 +0700

    Update and rename IR Remote Test.ino to Project 01.1 - IR Remote Test.ino

--- code/Project 01.1 - IR Remote Test.ino
+++ code/Project 01.1 - IR Remote Test.ino
@@ -1,7 +1,13 @@
+/*
+Project 01  : Membuat Sensor Infrared di Server
+Tujuan      : Agar Server dapat di ON OFF secara Online via Bardi Smart IR Receiver
+Fungsi Code : Test IR Remote ke Arduino dengan Boud Rate ke 9600
+*/
+
 // Menambahkan Library IR Remote by Ken Shirriff
 #include <IRremote.h>
 
-// Set pin dalam Arduino ke IR Remote
+// Set pin Digital dalam Arduino ke IR Remote
 int IR_PIN = 2;
 
 IRrecv irDetect(IR_PIN);
